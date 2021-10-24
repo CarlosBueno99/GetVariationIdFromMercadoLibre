ddc1058b12c27718c5058d7c88a4d0f6464dffee
CarlosBueno99 <carlosbueno.contato@gmail.com>
2021-10-24 20:41:16 -0300

    now you can feed it a spreadsheet with the MLB ids

--- gettingVariationCodesFromMLid.py
+++ gettingVariationCodesFromMLid.py
@@ -30,35 +30,57 @@ data = res.read()
 response = json.loads(data)
 
 
-with open( csvDestination , 'w', newline='') as csvout:
+with open( csvDestination , 'w',encoding="utf-8", newline='') as csvout:
     writer = csv.writer(csvout)
-    writer.writerow(["Tray-Pai","MLB-Pai","MLB-filho","Value-name"])
+
+    #define o cabeçalho do arquivo
+    writer.writerow(["Tray-Pai","MLB-Pai","MLB-filho","Nome-produto","Var1","var2","var3","Nome-concatenado"])
     with open('csvIn.csv', newline='') as csvin:
         reader = csv.DictReader(csvin)
         for row in reader:
-            
-            
-            
-            print(row['codigo_padre_plataforma'],"|"+"|", row['codigo_MLB_padre'])
+            conn.request("GET", "/items/"+row['codigo_MLB_padre'], payload, headers)
+            res = conn.getresponse()
+            data = res.read()
+            response = json.loads(data)
+            product_name = response["title"]
 
-
-
-print("CSV criado com sucesso:", csvDestination+ csvName)
-
-
-
-for variation in response["variations"]:
+            #this step goes into the response and grabs the desired information from Mercado Libre's API
+            for variation in response["variations"]:
     
-    print(variation["id"])
+                print(variation["id"])
+                print(row['codigo_MLB_padre'])
+
+                
+                
+
+                
+                if len(variation["attribute_combinations"][0]["value_name"].split("/")) == 3:
+                    vari1 = variation["attribute_combinations"][0]["value_name"].split("/")[0].strip()
+                    vari2 = variation["attribute_combinations"][0]["value_name"].split("/")[1].strip()
+                    vari3 = variation["attribute_combinations"][0]["value_name"].split("/")[2].strip()
+
+                    #essa com 3
+                    nome_concatenado = product_name+" cor:"+vari1+";Tamanho:"+vari2+"vari3:"+vari3
+                elif len(variation["attribute_combinations"][0]["value_name"].split("/")) == 2:
+                    vari1 = variation["attribute_combinations"][0]["value_name"].split("/")[0].strip()
+                    vari2 = variation["attribute_combinations"][0]["value_name"].split("/")[1].strip()
+                    vari3 = "N/A"
+
+                    #essa com 2
+                    nome_concatenado = product_name+" cor:"+vari1+";Tamanho:"+vari2
+                else:
+                    vari1 = variation["attribute_combinations"][0]["value_name"].split("/")[0].strip()
+                    vari2 = "N/A"
+                    vari3 = "N/A"
+
+                    #Essa com 1
+                    nome_concatenado = product_name+" cor:"+vari1
+                
+                
+
+                print(nome_concatenado)
 
 
-    print(variation["attribute_combinations"][0]["value_name"].split(" / "))
+                writer.writerow([row['codigo_padre_plataforma'],row['codigo_MLB_padre'], variation["id"] ,product_name,vari1,vari2,vari3,nome_concatenado])
+                
 
-    cor_vari = variation["attribute_combinations"][0]["value_name"].split(" / ")[0]
-    tam_vari = variation["attribute_combinations"][0]["value_name"].split(" / ")[1]
-    print(cor_vari,tam_vari)
-    
-        
-        
-
-    
